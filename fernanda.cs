1067e1a35f2998dad89100c2b5514cbc050af611
bacelarfc <fernandacbacelar@gmail.com>
2022-03-17 23:27:57 +0100

    Added hello world homework in C#

--- fernanda.cs
+++ fernanda.cs
@@ -1,5 +1,3 @@
- static void Main(string[] args)
-        {
-            System.Console.WriteLine("Hello World!");
-        }
-    }
+ static void Main(string[] args) {
+ System.Console.WriteLine("Hello World!!!");
+ }
